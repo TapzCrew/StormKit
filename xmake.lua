a356a609b9ab439d194ccb79faed1daf9ef9657b
Arthur <arthur.laurent4@gmail.com>
2022-05-18 23:40:26 +0200

    [XMake] Add support of unity build

--- xmake.lua
+++ xmake.lua
@@ -45,8 +45,8 @@ set_allowedplats("windows", "mingw", "linux", "macosx")
 set_allowedarchs("windows|x64", "mingw|x86_64", "linux|x86_64", "macosx|x86_64")
 
 package("StormKit")
-    option("enable_cxx20_modules")
-        set_default(use_cxx20_modules)
+    option("unity_build")
+        set_default(false)
         set_showmenu(true)
         add_defines("STORMKIT_CXX20_MODULES")
     option_end()
@@ -153,9 +153,9 @@ package("StormKit")
         end
     end
 
-    if has_config("enable_cxx20_modules") then
-        add_defines("STORMKIT_CXX20_MODULES")
-    else
+    if has_config("unity_build") then
+        add_rules("c.unity_build")
+        add_rules("c++.unity_build")
         add_defines("module=")
         add_defines("_ALLOW_KEYWORD_MACROS")
     end
