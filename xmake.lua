19ec27dea4771a3f5475f39cb70c80e8452b0cde
Arthur <arthur.laurent4@gmail.com>
2022-05-20 17:37:20 +0200

    Revert "[XMake] Enable 32 builds"
    This reverts commit 6e98508f8ec590139f86f9920a2fea535256e9cd.

--- xmake.lua
+++ xmake.lua
@@ -42,7 +42,7 @@ end
 
 set_allowedmodes(allowedmodes)
 set_allowedplats("windows", "mingw", "linux", "macosx")
-set_allowedarchs("windows|x64", "mingw|x86_64", "linux|x86_64", "macosx|x86_64", "windows|x86", "mingw|i386", "linux|x86")
+set_allowedarchs("windows|x64", "mingw|x86_64", "linux|x86_64", "macosx|x86_64")
 
 package("StormKit")
     option("unity_build")
