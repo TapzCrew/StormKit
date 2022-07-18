0318280e90cfaaf43f9ce5773145d1e92c445346
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-07-18 17:32:54 +0200

    [core] Fix std format detection

--- core/include/stormkit/core/FormatMacro.hpp
+++ core/include/stormkit/core/FormatMacro.hpp
@@ -4,7 +4,8 @@
 
 #pragma once
 
-#if defined(__cpp_lib_format)
+//#if defined(__cpp_lib_format) && __cpp_lib_format >= 202110L
+#if __has_include(<format>)
     #define STORMKIT_HAS_STL_FORMAT
 #elif __has_include(<fmt/format.h>)
     #define STORMKIT_HAS_FMT_FORMAT
@@ -15,14 +16,14 @@
 
 #ifdef STORMKIT_HAS_STL_FORMAT
     #define FORMATTER(TYPE, OUTPUT_STR, ...)                        \
-        inline auto toString(const TYPE &data) noexcept {           \
+        inline auto toString(const TYPE& data) noexcept {           \
             return stormkit::core::format(OUTPUT_STR, __VA_ARGS__); \
         }
 
     #define CUSTOM_FORMAT(TYPE, METHOD)                            \
         template<>                                                 \
         struct std::formatter<TYPE>: std::formatter<std::string> { \
-            auto format(const TYPE &data, format_context &ctx) {   \
+            auto format(const TYPE& data, format_context& ctx) {   \
                 std::ranges::copy(METHOD, ctx.out());              \
                 return ctx.out();                                  \
             }                                                      \
@@ -30,14 +31,14 @@
     #define CUSTOM_FORMAT_STRING_VIEW(TYPE, METHOD)                     \
         template<>                                                      \
         struct std::formatter<TYPE>: std::formatter<std::string_view> { \
-            auto format(const TYPE &data, format_context &ctx) {        \
+            auto format(const TYPE& data, format_context& ctx) {        \
                 std::ranges::copy(METHOD, ctx.out());                   \
                 return ctx.out();                                       \
             }                                                           \
         };
 #elif defined(STORMKIT_HAS_FMT_FORMAT)
     #define FORMATTER(TYPE, OUTPUT_STR, ...)                        \
-        inline auto toString(const TYPE &data) noexcept {           \
+        inline auto toString(const TYPE& data) noexcept {           \
             return stormkit::core::format(OUTPUT_STR, __VA_ARGS__); \
         }
 
@@ -45,7 +46,7 @@
         template<>                                                                     \
         struct fmt::formatter<TYPE>: formatter<std::string> {                          \
             template<typename FormatContext>                                           \
-            auto format(const TYPE &data, FormatContext &ctx) -> decltype(ctx.out()) { \
+            auto format(const TYPE& data, FormatContext& ctx) -> decltype(ctx.out()) { \
                 auto foo = METHOD;                                                     \
                 std::copy(std::begin(foo), std::end(foo), ctx.out());                  \
                 return ctx.out();                                                      \
@@ -56,7 +57,7 @@
         template<>                                                                     \
         struct fmt::formatter<TYPE>: formatter<std::string_view> {                     \
             template<typename FormatContext>                                           \
-            auto format(const TYPE &data, FormatContext &ctx) -> decltype(ctx.out()) { \
+            auto format(const TYPE& data, FormatContext& ctx) -> decltype(ctx.out()) { \
                 auto foo = METHOD;                                                     \
                 std::copy(std::begin(foo), std::end(foo), ctx.out());                  \
                 return ctx.out();                                                      \
