b7d4cc082a5afe2e959e34a8b767a3d87419cc7e
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-02-03 03:10:23 +0100

    rename Main files

---
+++ src/Main/iOS/Main-iOS.mm
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+auto main(int argc, char *argv[]) -> int {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    }
+}
--- src/Main/iOS/Main.mm
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-auto main(int argc, char *argv[]) -> int {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, @"AppDelegate");
-    }
-}
