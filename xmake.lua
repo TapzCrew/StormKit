710001b03558da7b954b353171516814ab3082f8
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-02-03 02:51:07 +0100

    fix frozen package

--- xmake.lua
+++ xmake.lua
@@ -58,6 +58,7 @@ modules = {
     deps = { "stormkit-core" },
     custom = function()
         add_cxflags("-Wno-main")
+        set_strip("debug")
     end
   },
   wsi = {
@@ -185,11 +186,11 @@ package("frozen", function()
   set_description("A header-only, constexpr alternative to gperf for C++14 users")
   set_license("Apache-2.0")
 
-  -- set_urls("https://github.com/Arthapz/frozen.git")
-  set_sourcedir("../frozen")
+  set_urls("https://github.com/Arthapz/frozen.git")
+  -- set_sourcedir("../frozen")
 
   on_install(function(package)
-    import("package.tools.xmake").install(package, { enable_module = true, enable_std_import = true })
+    import("package.tools.xmake").install(package, { enable_module = true, enable_std_import = true, enable_tests = false, enable_benchmark = false })
   end)
 end)
 
