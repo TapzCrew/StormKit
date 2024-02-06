67670277e162aa32afca5be6b0761e55eb172e96
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-02-06 21:21:10 +0100

    initial work for swapchains

---
+++ src/Gpu/Resource/Swapchain.cpp
@@ -0,0 +1,17 @@
+module stormkit.Gpu;
+
+import std;
+
+import stormkit.Core;
+
+import stormkit.Gpu.Vulkan;
+
+import :Core;
+import :Resource.Swapchain;
+
+namespace stormkit::gpu {
+    /////////////////////////////////////
+    /////////////////////////////////////
+    // Swapchain::Swapchain(std::string app_name, bool enable_validation, Tag) {
+    // }
+}
