5d1a6ec43860e9080f36126fe694ac6d4d07b06d
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-02-08 15:01:42 +0100

    .

--- src/Engine/Renderer/RenderSurface.cpp
+++ src/Engine/Renderer/RenderSurface.cpp
@@ -10,12 +10,21 @@ import stormkit.Gpu;
 import :Renderer;
 
 namespace stormkit::engine {
+    /////////////////////////////////////
+    /////////////////////////////////////
     RenderSurface::RenderSurface(const gpu::Instance& instance,
                                  const gpu::Device&   device,
                                  const wsi::Window&   window,
                                  Tag) {
         gpu::Surface::createFromWindow(instance, window)
             .transform(core::monadic::set(m_surface))
+            .and_then(std::bind_front(&RenderSurface::doInitSwapchain, this))
             .transform_error(core::throwError());
+    }
+
+    /////////////////////////////////////
+    /////////////////////////////////////
+    auto RenderSurface::doInitSwapchain() noexcept -> gpu::Expected<gpu::Swapchain> {
+        return {};
     }
 } // namespace stormkit::engine
