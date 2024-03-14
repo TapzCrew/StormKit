7b03fd448821e4859f27c7a395406cd2000847e6
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-03-14 17:28:04 +0100

    update frozen package

--- xmake/frozen.lua
+++ xmake/frozen.lua
@@ -1,14 +1,19 @@
 package("frozen", function()
-  set_kind("library")
-  set_homepage("https://github.com/serge-sans-paille/frozen")
-  set_description("A header-only, constexpr alternative to gperf for C++14 users")
-  set_license("Apache-2.0")
+	set_kind("library")
+	set_homepage("https://github.com/serge-sans-paille/frozen")
+	set_description("A header-only, constexpr alternative to gperf for C++14 users")
+	set_license("Apache-2.0")
 
-  set_urls("https://github.com/Arthapz/frozen.git")
-  -- set_sourcedir("../frozen")
+	set_urls("https://github.com/Arthapz/frozen.git")
 
-  on_install(function(package)
-    import("package.tools.xmake").install(package, { enable_module = true, enable_std_import = true, enable_tests = false, enable_benchmark = false })
-  end)
+	add_configs("modules", { default = false, type = "boolean" })
+	add_configs("std_import", { default = false, type = "boolean" })
+
+	on_install(function(package)
+		local configs = {
+			modules = package:config("modules"),
+			std_import = package:config("std_import"),
+		}
+		import("package.tools.xmake").install(package, configs)
+	end)
 end)
-
