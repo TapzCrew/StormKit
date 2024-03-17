f69e4a407b8b26dad4b88b8434a4c670ab882988
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-03-17 20:18:36 +0100

    [Gpu] improve and fix pipeline creation

--- src/Gpu/Execution/Pipeline.cpp
+++ src/Gpu/Execution/Pipeline.cpp
@@ -17,164 +17,162 @@ import :Execution.RasterPipelineState;
 namespace stormkit::gpu {
     /////////////////////////////////////
     /////////////////////////////////////
-    auto Pipeline::doInitRasterPipeline(const Device&                    device,
-                                        const PipelineLayout&            layout,
-                                        const RenderPass&                render_pass,
+    auto Pipeline::doInitRasterPipeline(const Device&                      device,
+                                        const PipelineLayout&              layout,
+                                        const RenderPass&                  render_pass,
                                         MaybeBorrowed<const PipelineCache> pipeline_cache) noexcept
         -> VulkanExpected<void> {
         const auto& state = as<RasterPipelineState>(m_state);
 
+        // clang-format off
         const auto binding_descriptions =
-            state.vertex_input_state.binding_descriptions |
-            std::views::transform([](auto&& binding_description) noexcept {
-                return vk::VertexInputBindingDescription {}
-                    .setBinding(binding_description.binding)
-                    .setStride(binding_description.stride)
-                    .setInputRate(narrow<vk::VertexInputRate>(binding_description.input_rate));
-            }) |
-            std::ranges::to<std::vector>();
+            state.vertex_input_state.binding_descriptions 
+            | std::views::transform([](auto&& binding_description) static noexcept {
+                return vk::VertexInputBindingDescription {
+                    .binding   = binding_description.binding,
+                    .stride    = binding_description.stride,
+                    .inputRate = narrow<vk::VertexInputRate>(binding_description.input_rate)
+
+                };
+            }) 
+            | std::ranges::to<std::vector>();
 
         const auto attribute_descriptions =
-            state.vertex_input_state.input_attribute_descriptions |
-            std::views::transform([](auto&& input_attribute_description) noexcept {
-                return vk::VertexInputAttributeDescription {}
-                    .setLocation(input_attribute_description.location)
-                    .setBinding(input_attribute_description.binding)
-                    .setFormat(narrow<vk::Format>(input_attribute_description.format))
-                    .setOffset(input_attribute_description.offset);
-            }) |
-            std::ranges::to<std::vector>();
+            state.vertex_input_state.input_attribute_descriptions 
+            | std::views::transform([](auto&& input_attribute_description) static noexcept {
+                return vk::VertexInputAttributeDescription {
+                    .location = input_attribute_description.location,
+                    .binding  = input_attribute_description.binding,
+                    .format   = narrow<vk::Format>(input_attribute_description.format),
+                    .offset   = input_attribute_description.offset
+                };
+            }) 
+            | std::ranges::to<std::vector>();
 
         const auto vertex_input_info = vk::PipelineVertexInputStateCreateInfo {}
                                            .setVertexBindingDescriptions(binding_descriptions)
                                            .setVertexAttributeDescriptions(attribute_descriptions);
 
         const auto input_assembly =
-            vk::PipelineInputAssemblyStateCreateInfo {}
-                .setTopology(narrow<vk::PrimitiveTopology>(state.input_assembly_state.topology))
-                .setPrimitiveRestartEnable(state.input_assembly_state.primitive_restart_enable);
+            vk::PipelineInputAssemblyStateCreateInfo {
+                .topology = narrow<vk::PrimitiveTopology>(state.input_assembly_state.topology),
+                .primitiveRestartEnable = state.input_assembly_state.primitive_restart_enable
+            };
 
-        const auto viewports = state.viewport_state.viewports |
-                               std::views::transform([](auto&& viewport) noexcept {
-                                   return vk::Viewport {}
-                                       .setX(viewport.position.x)
-                                       .setY(viewport.position.y)
-                                       .setWidth(viewport.extent.width)
-                                       .setHeight(viewport.extent.height)
-                                       .setMinDepth(viewport.depth.x)
-                                       .setMaxDepth(viewport.depth.y);
-                               }) |
-                               std::ranges::to<std::vector>();
+        const auto viewports = state.viewport_state.viewports 
+                               | std::views::transform(core::monadic::as<vk::Viewport>())
+                               | std::ranges::to<std::vector>();
 
-        const auto scissors =
-            state.viewport_state.scissors | std::views::transform([](auto&& scissor) noexcept {
-                return vk::Rect2D {}
-                    .setOffset(vk::Offset2D { scissor.offset.x, scissor.offset.y })
-                    .setExtent(vk::Extent2D { scissor.extent.width, scissor.extent.height });
-            }) |
-            std::ranges::to<std::vector>();
+        const auto scissors = state.viewport_state.scissors 
+                               | std::views::transform(core::monadic::as<vk::Rect2D>())
+                               | std::ranges::to<std::vector>();
 
         const auto viewport_state =
             vk::PipelineViewportStateCreateInfo {}.setViewports(viewports).setScissors(scissors);
 
         const auto rasterizer =
-            vk::PipelineRasterizationStateCreateInfo {}
-                .setDepthClampEnable(state.rasterization_state.depth_clamp_enable)
-                .setRasterizerDiscardEnable(state.rasterization_state.rasterizer_discard_enable)
-                .setPolygonMode(narrow<vk::PolygonMode>(state.rasterization_state.polygon_mode))
-                .setCullMode(narrow<vk::CullModeFlagBits>(state.rasterization_state.cull_mode))
-                .setFrontFace(narrow<vk::FrontFace>(state.rasterization_state.front_face))
-                .setLineWidth(state.rasterization_state.line_width);
+            vk::PipelineRasterizationStateCreateInfo {
+                .depthClampEnable = state.rasterization_state.depth_clamp_enable,
+                .rasterizerDiscardEnable = state.rasterization_state.rasterizer_discard_enable,
+                .polygonMode = narrow<vk::PolygonMode>(state.rasterization_state.polygon_mode),
+                .cullMode = toVkFlags<vk::CullModeFlagBits>(state.rasterization_state.cull_mode),
+                .frontFace = narrow<vk::FrontFace>(state.rasterization_state.front_face),
+                .lineWidth = state.rasterization_state.line_width
+            };
 
         const auto multisampling =
-            vk::PipelineMultisampleStateCreateInfo {}
-                .setRasterizationSamples(
-                    narrow<vk::SampleCountFlagBits>(state.multisample_state.rasterization_samples))
-                .setSampleShadingEnable(state.multisample_state.sample_shading_enable);
+            vk::PipelineMultisampleStateCreateInfo {
+                .rasterizationSamples = 
+                    narrow<vk::SampleCountFlagBits>(state.multisample_state.rasterization_samples),
+                .sampleShadingEnable = state.multisample_state.sample_shading_enable
+            };
 
-        const auto blend_attachments =
-            state.color_blend_state.attachments |
-            std::views::transform([](auto&& attachment) noexcept {
-                return vk::PipelineColorBlendAttachmentState {}
-                    .setBlendEnable(attachment.blend_enable)
-                    .setSrcColorBlendFactor(
-                        narrow<vk::BlendFactor>(attachment.src_color_blend_factor))
-                    .setDstColorBlendFactor(
-                        narrow<vk::BlendFactor>(attachment.dst_color_blend_factor))
-                    .setColorBlendOp(narrow<vk::BlendOp>(attachment.color_blend_operation))
-                    .setSrcAlphaBlendFactor(
-                        narrow<vk::BlendFactor>(attachment.src_alpha_blend_factor))
-                    .setDstAlphaBlendFactor(
-                        narrow<vk::BlendFactor>(attachment.dst_alpha_blend_factor))
-                    .setAlphaBlendOp(narrow<vk::BlendOp>(attachment.alpha_blend_operation))
-                    .setColorWriteMask(
-                        narrow<vk::ColorComponentFlagBits>(attachment.color_write_mask));
-            }) |
-            std::ranges::to<std::vector>();
+        const auto blend_attachments = state.color_blend_state.attachments
+             | std::views::transform([](auto&& attachment) static noexcept {
+                return vk::PipelineColorBlendAttachmentState {
+                    .blendEnable         = attachment.blend_enable,
+                    .srcColorBlendFactor = narrow<vk::BlendFactor>(attachment.src_color_blend_factor),
+                    .dstColorBlendFactor = narrow<vk::BlendFactor>(attachment.dst_color_blend_factor),
+                    .colorBlendOp        = narrow<vk::BlendOp>(attachment.color_blend_operation),
+                    .srcAlphaBlendFactor = narrow<vk::BlendFactor>(attachment.src_alpha_blend_factor),
+                    .dstAlphaBlendFactor = narrow<vk::BlendFactor>(attachment.dst_alpha_blend_factor),
+                    .alphaBlendOp        = narrow<vk::BlendOp>(attachment.alpha_blend_operation),
+                    .colorWriteMask      = toVkFlags<vk::ColorComponentFlagBits>(attachment.color_write_mask)
+                };
+            })
+            | std::ranges::to<std::vector>();
 
-        const auto color_blending =
-            vk::PipelineColorBlendStateCreateInfo {}
-                .setLogicOpEnable(state.color_blend_state.logic_operation_enable)
-                .setLogicOp(narrow<vk::LogicOp>(state.color_blend_state.logic_operation))
-                .setAttachments(blend_attachments)
-                .setBlendConstants({ state.color_blend_state.blend_constants[0],
-                                     state.color_blend_state.blend_constants[1],
-                                     state.color_blend_state.blend_constants[2],
-                                     state.color_blend_state.blend_constants[3] });
+        const auto color_blending = vk::PipelineColorBlendStateCreateInfo {
+                .logicOpEnable  = state.color_blend_state.logic_operation_enable,
+                .logicOp        = narrow<vk::LogicOp>(state.color_blend_state.logic_operation),
+        }
+        .setBlendConstants({ state.color_blend_state.blend_constants[0],
+                             state.color_blend_state.blend_constants[1],
+                             state.color_blend_state.blend_constants[2],
+                             state.color_blend_state.blend_constants[3] })
+        .setAttachments(blend_attachments);
+       
 
-        const auto states =
-            state.dynamic_state.dynamics |
-            std::views::transform([](auto&& s) noexcept { return narrow<vk::DynamicState>(s); }) |
-            std::ranges::to<std::vector>();
+        const auto states = state.dynamic_state.dynamics 
+            | std::views::transform(core::monadic::narrow<vk::DynamicState>())
+            | std::ranges::to<std::vector>();
 
         const auto dynamic_state = vk::PipelineDynamicStateCreateInfo {}.setDynamicStates(states);
 
-        const auto shaders = state.shader_state.shaders |
-                             std::views::transform([](auto&& shader) noexcept {
-                                 static auto NAME = "main";
-                                 return vk::PipelineShaderStageCreateInfo {}
-                                     .setStage(narrow<vk::ShaderStageFlagBits>(shader->type()))
-                                     .setModule(toVkHandle(shader))
-                                     .setPName(NAME);
-                             }) |
-                             std::ranges::to<std::vector>();
+        const auto shaders = state.shader_state.shaders
+                             | std::views::transform([](auto&& shader) static noexcept {
+                                 static constexpr auto NAME = "main";
+                                 return vk::PipelineShaderStageCreateInfo {
+                                     .stage  = narrow<vk::ShaderStageFlagBits>(shader->type()),
+                                     .module = toVkHandle(shader),
+                                     .pName  = NAME
+                                 };
+                             })
+                             | std::ranges::to<std::vector>();
 
-        const auto depth_stencil =
-            vk::PipelineDepthStencilStateCreateInfo {}
-                .setDepthTestEnable(state.depth_stencil_state.depth_test_enable)
-                .setDepthWriteEnable(state.depth_stencil_state.depth_write_enable)
-                .setDepthCompareOp(
-                    narrow<vk::CompareOp>(state.depth_stencil_state.depth_compare_op))
-                .setDepthBoundsTestEnable(state.depth_stencil_state.depth_bounds_test_enable)
-                .setStencilTestEnable(false)
-                .setMinDepthBounds(state.depth_stencil_state.min_depth_bounds)
-                .setMaxDepthBounds(state.depth_stencil_state.max_depth_bounds);
-
-        const auto create_info = vk::GraphicsPipelineCreateInfo {}
-                                     .setStages(shaders)
-                                     .setPVertexInputState(&vertex_input_info)
-                                     .setPInputAssemblyState(&input_assembly)
-                                     .setPViewportState(&viewport_state)
-                                     .setPRasterizationState(&rasterizer)
-                                     .setPMultisampleState(&multisampling)
-                                     .setPDepthStencilState(&depth_stencil)
-                                     .setPColorBlendState(&color_blending)
-                                     .setPDynamicState(&dynamic_state)
-                                     .setLayout(toVkHandle(layout))
-                                     .setRenderPass(toVkHandle(render_pass))
-                                     .setSubpass(0)
-                                     .setBasePipelineHandle(nullptr)
-                                     .setBasePipelineIndex(-1);
-
-        using PipelineCacheRef = Borrowed<const vk::raii::PipelineCache>;
-
-        const auto vk_pipeline_cache = [&pipeline_cache] -> const vk::raii::PipelineCache& {
-            if (pipeline_cache) return toRaiiVkHandle(*pipeline_cache);
-            return nullptr;
+        const auto depth_stencil = vk::PipelineDepthStencilStateCreateInfo {
+          .depthTestEnable       = state.depth_stencil_state.depth_test_enable,
+          .depthWriteEnable      = state.depth_stencil_state.depth_write_enable,
+          .depthCompareOp        = narrow<vk::CompareOp>(state.depth_stencil_state.depth_compare_op),  
+          .depthBoundsTestEnable = state.depth_stencil_state.depth_bounds_test_enable,
+          .stencilTestEnable     = false, 
+          .minDepthBounds        = state.depth_stencil_state.min_depth_bounds, 
+          .maxDepthBounds        = state.depth_stencil_state.max_depth_bounds
         };
 
+        const auto create_info = vk::GraphicsPipelineCreateInfo {
+         .pVertexInputState   = &vertex_input_info,
+         .pInputAssemblyState = &input_assembly,
+         .pViewportState      = &viewport_state,
+         .pRasterizationState = &rasterizer,
+         .pMultisampleState   = &multisampling,
+         .pDepthStencilState  = &depth_stencil,
+         .pColorBlendState    = &color_blending,
+         .pDynamicState       = &dynamic_state,
+         .layout              = toVkHandle(layout),
+         .renderPass          = toVkHandle(render_pass),
+         .subpass             = 0,
+         .basePipelineHandle  = nullptr,
+         .basePipelineIndex   = -1
+        }
+        .setStages(shaders);
+        // clang-format on
+
+        using PipelineCacheOpt = vk::Optional<const vk::raii::PipelineCache>;
+
+        // const auto vk_pipeline_cache = [&pipeline_cache] -> const vk::raii::PipelineCache& {
+        //     if (pipeline_cache) return toRaiiVkHandle(*pipeline_cache);
+        //     return nullptr;
+        // };
+
+        const auto vk_pipeline_cache =
+            core::either(pipeline_cache,
+                         core::monadic::map(monadic::toRaiiVkHandle(),
+                                            core::monadic::init<PipelineCacheOpt>()),
+                         core::monadic::init<PipelineCacheOpt>(nullptr));
+
         return toRaiiVkHandle(device)
-            .createGraphicsPipelines(vk_pipeline_cache(), create_info)
+            .createGraphicsPipelines(vk_pipeline_cache, create_info)
             .transform([this](auto&& pipelines) noexcept {
                 m_vk_pipeline = std::move(pipelines.front());
             });
